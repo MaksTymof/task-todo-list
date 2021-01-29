6ba748f1d0be00601b0cb9f49702034cf79c7ac2
MaksTymof <maksym198858@gmail.com>
2021-01-29 18:58:01 +0200

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <ToDoList msg="Welcome to My ToDoList.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ToDoList from '@/components/ToDoList.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    ToDoList
+  }
+}
+</script>
